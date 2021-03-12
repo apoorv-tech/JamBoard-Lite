d096cff39b32abfcc42dbc59254ce7f6d0fe43c9
saksham saxena <saksham12022002@gmail.com>
2021-03-12 17:29:45 +0530

    Routes All Bugs Resolved

--- routes/Jamboard/jamboard.js
+++ routes/Jamboard/jamboard.js
@@ -11,13 +11,38 @@ router.get('/',requireauth,async (req,res)=>{
     console.log(req.query._uid)
     const allusers = await User.find({})
     const allusers2 = await User2.find({})
-    res.render('Jamboard/index',{
-        fileused: "Jamboard",
-        users : allusers,
-        users2 : allusers2,
-        jambaord_id : req.query._id,
-        userid : req.query._uid
-    })
+    if (req.query._id==null) {
+        res.redirect('/dashboard?_uid='+res.locals.user._id)
+    }
+    else{
+        const jam1 = await Jamboard.findOne({_id: req.query._id}, function(err,result){
+            
+            console.log(result);
+            if (err) {
+                console.log("error is :"+err);
+            }
+            if (result) {
+                let users = result.users;
+                if (users.includes(res.locals.user._id)) {
+                    res.render('Jamboard/index',{
+                        fileused: "Jamboard",
+                        users : allusers,
+                        users2 : allusers2,
+                        jambaord_id : req.query._id,
+                        userid : res.locals.user._id
+                    })
+                }
+                else{
+                    res.redirect('/dashboard?_uid='+res.locals.user._id)
+                }
+            }
+            else{
+                res.redirect('/dashboard?_uid='+res.locals.user._id)
+            }
+        });
+        
+    }
+    
 })
 
 router.post('/',requireauth,async (req,res)=>{
@@ -27,21 +52,30 @@ router.post('/',requireauth,async (req,res)=>{
     let user = await User.findOne({email : req.body.add})
     if(user)
     {
+
         users = jam.users
-        users.push(user._id)
-        Jamboard.updateOne({'_id' : req.query._id},{$set: { 'users' : users}},function(err,res){
-            if(err) throw err
-        })
+        if(!(users.includes(user._id))){
+            users.push(user._id)
+            Jamboard.updateOne({'_id' : req.query._id},{$set: { 'users' : users}},function(err,res){
+                if(err) throw err
+            })
+        }
+        
+
+        
     }
     else {
         user = await User2.findOne({mail : req.body.add})
         users = jam.users
-        users.push(user._id)
-        Jamboard.updateOne({'_id' : req.query._id},{$set: { 'users' : users}},function(err,res){
-            if(err) throw err
-        })
+        if(!(users.includes(user._id))){
+            users.push(user._id)
+            Jamboard.updateOne({'_id' : req.query._id},{$set: { 'users' : users}},function(err,res){
+                if(err) throw err
+            })
+        }
+        
     }
-    res.redirect('/jamboard?_id='+req.query._id+'&_uid='+req.query._uid)
+    res.redirect('/jamboard?_id='+req.query._id+'&_uid='+res.locals.user._id)
 })
 
 module.exports =  router
