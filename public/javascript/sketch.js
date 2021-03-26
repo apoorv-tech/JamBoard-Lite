015a08608302d8741e6cc4bb35aca7f9215c8fdc
apoorv-tech <apoorvpriyadarsh1729@gmail.com>
2021-03-26 17:24:12 +0530

    final 1

--- public/javascript/sketch.js
+++ public/javascript/sketch.js
@@ -29,7 +29,7 @@ function setup(){
 	background(51)
 	console.log(jamid)
 	console.log(userid)
-	socket = io.connect('http://127.0.0.1:4000',{
+	socket = io.connect('https://jamboard-lite.herokuapp.com',{
 		query: {
 			_id: jamid,
 			_uid: userid 
